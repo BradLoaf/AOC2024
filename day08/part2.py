f1580f74de1440dd3e4558b873dca65e08fd6aca
Bradley Harvan <BHARVAN@MITRE.ORG>
2024-12-11 11:28:02 -0500

    day8-2

--- day08/part2.py
+++ day08/part2.py
@@ -1,5 +1,5 @@
 with open('src.txt', "r") as f:
-    lines = f.read().splitlines()
+    lines = [list(line) for line in f.read().splitlines()]
 
 antennas = {}
 for row in range(len(lines)):
@@ -14,6 +14,7 @@ antinodes = set()
 for freq, positions in antennas.items():
     n = len(positions)
     for i in range(n):
+        antinodes.add(positions[i])
         for j in range(i + 1, n):
             x1, y1 = positions[i]
             x2, y2 = positions[j]
@@ -21,11 +22,17 @@ for freq, positions in antennas.items():
             dx, dy = x2 - x1, y2 - y1
             
             x3, y3 = x1 - dx, y1 - dy
-            if 0 <= x3 < len(lines) and 0 <= y3 < len(lines[0]):
+            while 0 <= x3 < len(lines) and 0 <= y3 < len(lines[0]):
                 antinodes.add((x3, y3))
+                x3, y3 = x3 - dx, y3 - dy
             
             x4, y4 = x2 + dx, y2 + dy
-            if 0 <= x4 < len(lines) and 0 <= y4 < len(lines[0]):
+            while 0 <= x4 < len(lines) and 0 <= y4 < len(lines[0]):
                 antinodes.add((x4, y4))
+                x4, y4 = x4 + dx, y4 + dy
+for antinode in antinodes:
+    lines[antinode[0]][antinode[1]] = '#'
+for line in lines:
+    print(''.join(line))
 
 print(len(antinodes))
